6c9401fd1fcef4c79c2f17f8579d2a3be83b6957
hari207 <hari207@yahoo.com>
2020-06-01 23:10:17 +0530

    Update assignment2.R

--- assignment2.R
+++ assignment2.R
@@ -1,3 +1,27 @@
+#Introduction
+
+#This 2nd programming assignment 
+#will require you to write an R function that is able to cache potentially time-consuming computations. 
+#For example, taking the mean of a numeric vector is typically a fast operation. 
+#However, for a very long vector, it may take more time to compute the mean,  
+#if it has to be computed repeatedly . 
+#If the contents of a vector are not changing, it may make sense to cache the value of the mean so that when we need it again, 
+#it can be looked up in the cache rather than recomputed. 
+#In this Programming Assignment you will take advantage of the scoping rules of the R language 
+#and how they can be manipulated to preserve state inside of an R object.
+
+#  Example: Caching the Mean of a Vector
+
+
+
+#The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+#
+#    set the value 
+#    get the value
+#    set the value 
+#    get the value 
+
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   
@@ -21,6 +45,7 @@ makeCacheMatrix <- function(x = matrix()) {
        set_inverse = set_inverse,
        get_inverse = get_inverse)
 }
+#The first function, cacheSolve  
 
 cacheSolve <- function(x, ...) {
   i <- x$get_inverse()
